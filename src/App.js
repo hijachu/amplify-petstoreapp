f277ff7b3e3ed828c76933d8a0f93cf8dcf92530
YuChih Hu <YuChih.Hu@zyxel.com.tw>
2024-08-26 19:22:52 +0800

    Figma "Add Pet" component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { NavBar, Footer } from "./ui-components";
+// import { PetProfile } from "./ui-components";
+import { Pets } from "./ui-components";
 
 function App() {
+  // const petProfileOverride = {
+  //   Breed: {
+  //     color: "blue",
+  //   },
+  // };
+
+  const navbarOverrides = {
+    image: {
+      src: "https://img.icons8.com/color/50/000000/cat",
+    },
+    "Add Pet": {
+      style: {
+        cursor: "pointer"
+      },
+      onClick: () => {
+        alert("Hello");
+      },
+    },
+  };
+
   return (
     <div className="App">
+      <NavBar 
+        width={"100%"} 
+        overrides={navbarOverrides}
+        />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Pets itemsPerPage={1} currentPage={2} /> */}
+        <Pets
+          overrideItems={({ item, index }) => ({
+            overrides: {
+              Breed: { color: "blue" },
+              Button29766907: { onClick: () => alert(`${item.name}`) },
+            },
+          })}
+          />
+        {/* <PetProfile overrides={petProfileOverride} /> */}
       </header>
+      <Footer width={"100%"} />
     </div>
   );
 }
---
+++ src/ui-components/NavBar.d.ts
@@ -0,0 +1,35 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { Pet } from "../models";
+import { FlexProps, IconProps, ImageProps, TextProps, ViewProps } from "@aws-amplify/ui-react";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type NavBarOverridesProps = {
+    NavBar?: PrimitiveOverrideProps<FlexProps>;
+    "Frame 5"?: PrimitiveOverrideProps<FlexProps>;
+    "Amplify Mark"?: PrimitiveOverrideProps<ViewProps>;
+    Union?: PrimitiveOverrideProps<IconProps>;
+    "Add Pet"?: PrimitiveOverrideProps<TextProps>;
+    "Frame 321"?: PrimitiveOverrideProps<FlexProps>;
+    image?: PrimitiveOverrideProps<ImageProps>;
+} & EscapeHatchProps;
+export declare type NavBarProps = React.PropsWithChildren<Partial<FlexProps> & {
+    pet?: Pet;
+} & {
+    overrides?: NavBarOverridesProps | undefined | null;
+}>;
+export default function NavBar(props: NavBarProps): React.ReactElement;
